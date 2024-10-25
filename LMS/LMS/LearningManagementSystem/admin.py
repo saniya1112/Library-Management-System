d4ed97e786fff0a86d78a7824ac0d51f72c5ab42
saniya1112 <154654553+saniya1112@users.noreply.github.com>
2024-10-25 22:33:28 +0530

    Delete LMS/LMS directory

--- LMS/LMS/LearningManagementSystem/admin.py
+++
@@ -1,8 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from .models import Members, Book, Issue, OverdueReturn
-
-admin.site.register(Members)
-admin.site.register(Book)
-admin.site.register(Issue)
-admin.site.register(OverdueReturn)
